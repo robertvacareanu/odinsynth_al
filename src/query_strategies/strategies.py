6e4c667d1faa805e3a639902790db37f307480fe
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-11-14 20:54:55 -0700

    Prediction Entropy query strategy

--- src/query_strategies/strategies.py
+++ src/query_strategies/strategies.py
@@ -7,6 +7,7 @@ to the indices we want to select for annotations (as per AL
 paradigm))
 """
 import random
+from scipy.stats import entropy
 from typing import List
 
 
@@ -15,3 +16,14 @@ In this query implementation we just select random
 """
 def random_query(predictions: List[List[float]], k=5) -> List[int]:
     random.choices(list(range(len(predictions))), k=k)
+
+
+"""
+In this query implementation we select the top `k` by entropy
+Higher entropy means more uncertainty
+"""
+def prediction_entropy_query(predictions: List[List[float]], k=5) -> List[int]:
+    entropies = [entropy(x) for x in predictions]
+
+    entropies_and_indices = list(zip(range(len(entropies)), entropies))
+    return [x[0] for x in sorted(entropies_and_indices, key=lambda x: x[1], reverse=True)[:k]]
