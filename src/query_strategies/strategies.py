a26522b6b88820d6c018fe44c542f75ddbb89304
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-11-14 20:53:22 -0700

    Comments

--- src/query_strategies/strategies.py
+++ src/query_strategies/strategies.py
@@ -1,6 +1,17 @@
+"""
+This file contains the associated code for querying
+That is, given a list of datapoints, each with an 
+associated predictions (here modelled as a list
+of floats), we return a list of indices, corresponding
+to the indices we want to select for annotations (as per AL
+paradigm))
+"""
 import random
 from typing import List
 
 
-def random_query(predictions: List[List[float]], k=5):
+"""
+In this query implementation we just select random
+"""
+def random_query(predictions: List[List[float]], k=5) -> List[int]:
     random.choices(list(range(len(predictions))), k=k)
