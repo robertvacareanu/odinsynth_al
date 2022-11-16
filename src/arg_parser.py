719b3ac2932923731c90d86368fe2d829daa4476
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-11-15 21:56:33 -0700

    Query Strategy arg

--- src/arg_parser.py
+++ src/arg_parser.py
@@ -5,10 +5,10 @@ def base_parser(parent_parser):
     subparser.add_argument('--starting_size_ratio', type=float, default = 0.01)
     subparser.add_argument('--underlying_model', type=str, default="distilbert-base-uncased")
     subparser.add_argument('--number_of_new_examples', type=int, default=20)
-    subparser.add_argument('--number_of_al_iterations', type=str, default="distilbert-base-uncased")
+    subparser.add_argument('--number_of_al_iterations', type=int, default=10)
     subparser.add_argument('--epochs', type=int, default=10)
     subparser.add_argument('--learning_rate', type=float, default=2e-5)
-
+    subparser.add_argument('--query_strategy_function', type=str, default='random_query', choices=['random_query', 'prediction_entropy_query', 'breaking_ties_query', 'least_confidence_query'])
     return parent_parser
 
 
