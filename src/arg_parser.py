d48bdad2d10097dd0adc6ce4c4e36b40c2a42d2e
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-11-19 13:14:12 -0700

    Annotation strategy param

--- src/arg_parser.py
+++ src/arg_parser.py
@@ -9,6 +9,7 @@ def base_parser(parent_parser):
     subparser.add_argument('--epochs', type=int, default=10)
     subparser.add_argument('--learning_rate', type=float, default=2e-5)
     subparser.add_argument('--query_strategy_function', type=str, default='random_query', choices=['random_query', 'prediction_entropy_query', 'breaking_ties_query', 'least_confidence_query'])
+    subparser.add_argument('--annotation_strategy', type=str, default='sentence_level', choices=['sentence_level', 'entity_level', 'token_level'])
     return parent_parser
 
 
