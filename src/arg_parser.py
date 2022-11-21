81d1647dd7900c4a0cd41aa71422f5b32cbd193d
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-11-20 20:21:48 -0700

    Seed param

--- src/arg_parser.py
+++ src/arg_parser.py
@@ -2,6 +2,7 @@ import argparse
 
 def base_parser(parent_parser):
     subparser = parent_parser.add_argument_group("active_learning")
+    subparser.add_argument('--seed', type=int, default=1)
     subparser.add_argument('--starting_size_ratio', type=float, default = 0.01)
     subparser.add_argument('--underlying_model', type=str, default="distilbert-base-uncased")
     subparser.add_argument('--number_of_new_examples', type=int, default=20)
