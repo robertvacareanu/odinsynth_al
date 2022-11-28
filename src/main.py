f550879420c25194602e32375e9f8ca891e8ab64
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-11-28 10:16:58 -0700

    Multiple datasets. Fully supervised

--- src/main.py
+++ src/main.py
@@ -80,19 +80,22 @@ dataset_name_to_fn = {
 query_strategy_function = annotation_strategy_to_query_strategy_fn[args['annotation_strategy']][args['query_strategy_function']]
 query_random = annotation_strategy_to_query_strategy_fn[args['annotation_strategy']]['random_query']
 
-conll2003, label_to_id, id_to_label = get_conll2003()
+ner_dataset, label_to_id, id_to_label = dataset_name_to_fn['dataset_name']()
 
-starting_size = int(len(conll2003['train']) * args['starting_size_ratio'])
+if args['use_full_dataset']:
+    starting_size = len(ner_dataset['train'])
+else:
+    starting_size = int(len(ner_dataset['train']) * args['starting_size_ratio'])
 
-selected_indices = random.sample(range(0, len(conll2003['train'])), starting_size)
+selected_indices = random.sample(range(0, len(ner_dataset['train'])), starting_size)
 selected_indices_set = set(selected_indices)
 # This list holds what we have selected so far
-selected_dataset_so_far = dict([(x, ALAnnotation.from_line(line=conll2003['train'][x], sid=x)) for x in selected_indices])
+selected_dataset_so_far = dict([(x, ALAnnotation.from_line(line=ner_dataset['train'][x], sid=x)) for x in selected_indices])
 
 tokenizer = AutoTokenizer.from_pretrained(args['underlying_model'])
 
 # Tokenize everything
-tokenized_conll2003 = conll2003.map(lambda x: tokenize_and_align_labels(tokenizer, x), batched=True)
+tokenized_ner_dataset = ner_dataset.map(lambda x: tokenize_and_align_labels(tokenizer, x), batched=True)
 
 data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer, return_tensors="pt")
 metric = evaluate.load("seqeval")
@@ -119,7 +122,7 @@ for active_learning_iteration, number_of_new_examples, epochs, learning_rate in 
 
     # Create the new dataset using all the selected indices
     # Then, overwrite the labels by using only the labels we have annotated so far
-    data  = [{**conll2003["train"][x], 'ner_tags': selected_dataset_so_far[x].ner_tags} for x in selected_indices]
+    data  = [{**ner_dataset["train"][x], 'ner_tags': selected_dataset_so_far[x].ner_tags} for x in selected_indices]
     data  = Dataset.from_list(data).map(lambda x: tokenize_and_align_labels(tokenizer, x), batched=True, load_from_cache_file=False)
 
     # Use `Z` for 'O' to artificialy push it to the end of the sorted list
@@ -133,8 +136,8 @@ for active_learning_iteration, number_of_new_examples, epochs, learning_rate in 
         output_dir="./outputs",
         # evaluation_strategy="epoch",
         learning_rate=learning_rate,
-        per_device_train_batch_size=8,
-        per_device_eval_batch_size=16,
+        per_device_train_batch_size=args['train_batch_size'],
+        per_device_eval_batch_size=args['eval_batch_size'],
         num_train_epochs=epochs,
         weight_decay=0.01,
         save_strategy="no",
@@ -144,7 +147,7 @@ for active_learning_iteration, number_of_new_examples, epochs, learning_rate in 
         model=model,
         args=training_args,
         train_dataset=data,
-        # eval_dataset=tokenized_conll2003["validation"],
+        # eval_dataset=tokenized_ner_dataset["validation"],
         tokenizer=tokenizer,
         data_collator=data_collator,
         compute_metrics=lambda x: compute_metrics(x[0], x[1], id_to_label, metric=metric, verbose=True)
@@ -153,31 +156,28 @@ for active_learning_iteration, number_of_new_examples, epochs, learning_rate in 
     trainer.train()
 
     # unlabeled_dataset = []
-    # for i in range(len(tokenized_conll2003['train'])):
+    # for i in range(len(tokenized_ner_dataset['train'])):
         # if i not in selected_indices_set:
-            # unlabeled_dataset.append(tokenized_conll2003['train'][i])
+            # unlabeled_dataset.append(tokenized_ner_dataset['train'][i])
 
-    predictions = trainer.predict(tokenized_conll2003['train'])
+    if not args['use_full_dataset']:
+        predictions = trainer.predict(tokenized_ner_dataset['train'])
 
-    # Filter the [PAD] scores, the [CLS] scores, etc.
-    predictions_without_invalids = filter_invalid_token_predictions(predictions)
+        # Filter the [PAD] scores, the [CLS] scores, etc.
+        predictions_without_invalids = filter_invalid_token_predictions(predictions)
 
-    selected_dataset_so_far = dict(
-        query_strategy_function(predictions_without_invalids, k=number_of_new_examples, id_to_label=id_to_label, dataset_so_far=list(selected_dataset_so_far.items()), dataset=tokenized_conll2003['train'])
-    )
-    # selected_dataset_so_far = dict(
-        # query_strategy_function(predictions_without_invalids, k=int(3*number_of_new_examples/4), id_to_label=id_to_label, dataset_so_far=list(selected_dataset_so_far.items()), dataset=tokenized_conll2003['train'])
-    # )
-    # selected_dataset_so_far = dict(
-        # query_random(predictions_without_invalids, k=int(1*number_of_new_examples/4), id_to_label=id_to_label, dataset_so_far=list(selected_dataset_so_far.items()), dataset=tokenized_conll2003['train'])
-    # )
-    selected_indices_set = set(selected_dataset_so_far.keys())
-    # We do sorting to avoid any unpredictability that might have been added by the order in the dict 
-    # (i.e. set([1,2,3]) is not necessarily guaranteed to have the same order in between successive runs if PYTHONHASHSEED is not set)
-    # We let the dataloader do the shuffling during training and we ensure the same order of the dataset initially
-    selected_indices = sorted(list(selected_dataset_so_far.keys()))
+        selected_dataset_so_far = dict(
+            query_strategy_function(predictions_without_invalids, k=number_of_new_examples, id_to_label=id_to_label, dataset_so_far=list(selected_dataset_so_far.items()), dataset=tokenized_ner_dataset['train'])
+        )
 
-    predictions_val = trainer.predict(tokenized_conll2003["validation"])
+        selected_indices_set = set(selected_dataset_so_far.keys())
+        
+        # We do sorting to avoid any unpredictability that might have been added by the order in the dict 
+        # (i.e. set([1,2,3]) is not necessarily guaranteed to have the same order in between successive runs if PYTHONHASHSEED is not set)
+        # We let the dataloader do the shuffling during training and we ensure the same order of the dataset initially
+        selected_indices = sorted(list(selected_dataset_so_far.keys()))
+
+    predictions_val = trainer.predict(tokenized_ner_dataset["validation"])
     if args['verbose']:
         verbose_performance_printing(predictions_val, active_learning_iteration)
         
@@ -185,7 +185,7 @@ for active_learning_iteration, number_of_new_examples, epochs, learning_rate in 
         {
             'active_learning_iteration': active_learning_iteration,
             **predictions_val.metrics, 
-            # 'all_data_distribution'     : [(id_to_label[x[0]], x[1]) for x in Counter([y for x in conll2003['train'].select(selected_indices)['ner_tags'] for y in x]).items()],
+            # 'all_data_distribution'     : [(id_to_label[x[0]], x[1]) for x in Counter([y for x in ner_dataset['train'].select(selected_indices)['ner_tags'] for y in x]).items()],
             'annotation_strategy'       : args['annotation_strategy'],
             'query_strategy_function'   : args['query_strategy_function'],
             'number_of_al_iterations'   : args['number_of_al_iterations'],
