a90471c83af980f003193ffcd29c279e3dbb2fb1
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-11-28 21:21:16 -0700

    Arg checks

--- src/arg_checks.py
+++ src/arg_checks.py
@@ -8,11 +8,11 @@ This file contains various argument checks to ensure (as much as possible) a smo
 Side effects. If any check fails, raise a ValueError
 """
 def do_arg_checks(args):
-    if args['starting_size_ratio'] <= 0:
+    if args['starting_size_ratio'] <= 0 and args['use_ful_dataset'] == False:
         raise ValueError("The starting size ratio must be greater than 0. We cannot train with no examples. Is everything ok?")
-    if args['starting_size_ratio'] > 1.0:
+    if args['starting_size_ratio'] > 1.0 and args['use_ful_dataset'] == False:
         raise ValueError("The starting size ratio must be smaller or equal with 1. We cannot train with more examples than available in the dataset. Is everything ok?")
-    if any([x < 0 for x in args['number_of_new_examples']]):
+    if any([x < 0 for x in args['number_of_new_examples']]) and args['use_ful_dataset'] == False:
         raise ValueError("The number of new examples must be greater than 0. Is everything ok?")
     if any([x < 0 for x in args['epochs']]):
         raise ValueError("The number of epochs must be greater than 0. Is everything ok?")
@@ -20,9 +20,9 @@ def do_arg_checks(args):
         raise ValueError("The batch size must be greater than 0. Is everything ok?")
     if args['eval_batch_size'] < 0:
         raise ValueError("The batch size must be greater than 0. Is everything ok?")
-    if args['learning_rate'] < 0:
+    if any([x < 0 for x in args['learning_rate']]):
         raise ValueError("The learning rate must be greater than 0. Is everything ok?")
-    if args['learning_rate'] > 0.1:
+    if any([x > 0.1 for x in args['learning_rate']]):
         raise ValueError("While not exactly wrong, the learning rate seems very big. Is everything ok?")
 
 
