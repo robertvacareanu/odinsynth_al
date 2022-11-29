98e5cd30d6c496b9159e0abb48b415e02a7ea876
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-11-29 12:46:43 -0700

    Range

--- src/utils.py
+++ src/utils.py
@@ -252,7 +252,7 @@ class ALAnnotation:
             # We iterate over ecah token
             # If that token is inside the matches, we add it only if it is annotated (i.e. its label is not `-100`)
             # Otherwise, we drop it
-            for (i, token, al_annotated_ner_tag) in zip(range(len(self.al_annotated_ner_tags), self.original_dict['tokens'], self.al_annotated_ner_tags)):
+            for (i, token, al_annotated_ner_tag) in zip(range(len(self.al_annotated_ner_tags)), self.original_dict['tokens'], self.al_annotated_ner_tags):
                 if i in matches_indices:
                     if al_annotated_ner_tag != -100:
                         tokens.append(token)
