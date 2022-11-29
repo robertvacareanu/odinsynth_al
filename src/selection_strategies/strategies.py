598b4b8278381565e13661e4e9da9fc5d23aa20b
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-11-29 08:54:49 -0700

    Initial data selection strategies

--- src/selection_strategies/strategies.py
+++ src/selection_strategies/strategies.py
@@ -6,9 +6,10 @@ dataset than simply randomly sampling
 
 import random
 import numpy as np 
-import torch
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction import text
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.metrics import pairwise_distances_argmin_min
+from sklearn.metrics import pairwise_distances
 
 def random_initial_dataset_sampling(train_text, **kwargs):
     starting_size    = kwargs['starting_size']
@@ -36,31 +37,36 @@ def tfidf_initial_dataset_sampling(train_text, **kwargs):
     starting_size = kwargs['starting_size']
     topk_size     = kwargs.get('top_k_size', 5)
 
-    # stopwords = text.ENGLISH_STOP_WORDS
-
-    # original_index_map = [(i, x) for (i, x) in enumerate(train_text)]
-    # filtered_index_map = [x for x in original_index_map if any(y not in stopwords for y in x[1].split(' '))]
-    # filtered_index_to_original_index = dict([(i, x[0]) for (i, x) in enumerate(filtered_index_map)])
-    # filtered_text = [x[1] for x in filtered_index_map]
-    # print(filtered_index_map)
-
     vectorizer = TfidfVectorizer(stop_words = 'english', ngram_range=(1,2), norm=None)
-    X = vectorizer.fit_transform(train_text)
-    # means = []
-    # for i in range(X.shape[0]):
-    #     # print(train_text[i])
-    #     # print(X.getrow(i).data)
-    #     # print(top_k_mean(X.getrow(i).data, topk_size))
-    #     # exit()
-    #     m = top_k_mean(X.getrow(i).data, topk_size)
-    #     means.append(m)
-    # means = np.array(means)
+
+    X     = vectorizer.fit_transform(train_text)
     means = np.array([top_k_mean(X.getrow(i).data, topk_size) for i in range(X.shape[0])])
 
     new_starting_size = int(0.8 * starting_size)
     ids   = np.argpartition(means, -starting_size)[-starting_size:]
-    # ids   = [filtered_index_to_original_index[x] for x in ids]
-    # exit()
+
     return ids.tolist()
+
+
+def tfidf_kmeans_initial_dataset_sampling(train_text, **kwargs):
+
+    starting_size = kwargs['starting_size']
+    vectorizer = TfidfVectorizer(stop_words = 'english', ngram_range=(1,2), norm=None)
+
+    X     = vectorizer.fit_transform(train_text)
+
+    mbkm = MiniBatchKMeans(n_clusters=starting_size, init_size=1024, batch_size=2048, random_state=1).fit(X)
+
+    indices     = []
+    indices_set = set()
+    for cluster in mbkm.cluster_centers_:
+        distances = pairwise_distances(X, cluster.reshape(1, -1), metric='cosine')
+        distances = np.delete(distances, indices)
+        argmax = distances.argmax()
+        indices.append(argmax)
+        indices_set.add(argmax)
+
+    return indices
+
 
 
