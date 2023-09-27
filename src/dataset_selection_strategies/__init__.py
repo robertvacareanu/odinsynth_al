314d9cca9f584fa73a0a20a4a8b1a93f95ae52da
Robert Vacareanu <robert.vacareanu@gmail.com>
2023-09-27 09:44:23 +0000

    Comments

---
+++ src/dataset_selection_strategies/__init__.py
@@ -0,0 +1,5 @@
+"""
+This module contains implementations of different strategies to select the initial dataset
+As per Active Learning methodology, you start with an initial dataset before starting the Active Learning Loop.
+This module is about how to select that initial dataset. The simplest approach would be just to randomly sampling, but there might be better strategies available.
+"""
