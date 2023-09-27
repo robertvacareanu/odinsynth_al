dbfde6c741c56a654acd1ed4606ca38cb5bda9c0
Robert Vacareanu <robert.vacareanu@gmail.com>
2023-09-27 12:42:32 +0000

    Refactor

---
+++ src/query_strategies/data_modeling.py
@@ -0,0 +1,24 @@
+"""
+Data modeling for query strategies
+Concretely, we might want more information from a query strategy
+This class allows us to wrap everything we want/need here
+
+Regarding the design choice of using a `@dataclass` instead of a Dict
+"""
+
+from dataclasses import dataclass
+from typing import Union, Tuple, List
+
+from src.utils import ALAnnotation
+
+@dataclass
+class QueryStrategyOutput:
+    selections             : Union[List[int], List[Tuple[int, List[int]]]]
+    """What was selected to be annotated"""
+    
+    selected_dataset_so_far: List[Tuple[int, ALAnnotation]]
+    """The dataset selected so far"""
+
+    resulting_dataset      : List[Tuple[int, ALAnnotation]]
+    """The dataset after we added the current selections"""
+    
